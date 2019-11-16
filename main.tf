2f5e512ea2da28764b26013854589763625e5d7c
Henrik Larsen <henrikvlarsen@hotmail.com>
2019-11-16 21:02:46 +0100

    More comments..

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
 # baseline from https://www.terraform.io/docs/providers/azurerm/r/kubernetes_cluster.html
 
+#other inspirations
+#https://github.com/Azure/terraform-azurerm-aks
+#https://www.hashicorp.com/blog/kubernetes-cluster-with-aks-and-terraform/
+
+
 #mrg = my resource group - must not exist already according to above page
 resource "azurerm_resource_group" "mrg" {
   name     = "hvl71tfcaksrg"
@@ -15,17 +20,20 @@ resource "azurerm_kubernetes_cluster" "maks" {
 
   agent_pool_profile {
     name            = "default"
-    count           = 1
+    count           = 2
     vm_size         = "Standard_D1_v2"
     os_type         = "Linux"
     os_disk_size_gb = 30
   }
 
+
+  
+
   #override these in the terraform cloud UI. Remember to setup client_secret as.. secret
   #
   #https://docs.microsoft.com/en-us/azure/active-directory/develop/howto-create-service-principal-portal
   #https://www.terraform.io/docs/providers/azurerm/guides/service_principal_client_secret.html
-  
+
   service_principal {
     client_id     = "00000000-0000-0000-0000-000000000000"
     client_secret = "00000000000000000000000000000000"
