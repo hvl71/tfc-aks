3edcd79f326a1a7e0cfe9826e15ba9bfb8a82f47
Henrik Larsen <henrikvlarsen@hotmail.com>
2019-11-19 23:34:39 +0100

    are variables.tf needed at all?

--- variables.tf
+++
@@ -1,8 +0,0 @@
-variable "admin_username" {
-  default     = "azureuser"
-  description = "The username of the local administrator to be created on the Kubernetes cluster"
-}
-
-variable "admin_password" {
-  description = "The password of the local administrator to be created on the Kubernetes cluster"
-}
